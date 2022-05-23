c67b54fb6afa81fb52bb72b9d470cf8515919505
Sam <salonso@grupopm.mx>
2022-05-23 16:03:27 -0500

    se esta uniendo las tablas para presentarse en el explorador

--- app.js
+++ app.js
@@ -1,30 +1,85 @@
 const express = require('express');
 const Playlist = require('./models/playlist');
+const Artist = require('./models/artist');
+const Album = require('./models/album');
+const Track = require('./models/track');
+const Sequelize = require('sequelize');
 
-//const Sequelize = require('sequelize');
+
+const { Op } = Sequelize;
+
 const app = express();
-//const sequelize = new Sequelize('sqlite:chinook.db');
+
+ Artist.hasMany(Album,{
+    foreignKey:'ArtistId'
+ });
+
+ Album.belongsTo (Artist,{
+    foreignKey:'ArtistId'
+ });
+
+ Playlist.belongsToMany(Track,{
+    through: 'playlist_track',
+    foreignKey: 'PlaylistId',
+    timesstamps:false
+ })
 
 app.get('/api/playlists',function(request,response){
+let filter = {};
+let {q} = request.query;
 
-    
-    Playlist.findAll().then((playlists) => {
+     if (q){
+         filter ={
+            where:{
+                name:{
+                    [Op.like]: `${q}%`
+                }
+            }
+         };
+     }
+ 
+    Playlist.findAll(filter).then((playlists) => {
         response.json(playlists);
     });
 });
 
 app.get('/api/playlists/:id',function(request,response){
     let { id } = request.params;
-    Playlist.findByPk(id).then((playlist) => {
+    Playlist.findByPk(id,{
+    
+    }).then((playlist) => {
         if (playlist){
             response.json(playlist);
         } else {
             response.status(404).send();
         }
-
-        
     });
 });
 
+app.get('/api/artists/:id',function(request,response){
+    let { id } = request.params;
+    Artist.findByPk(id, {
+        include:[Album]
+    }).then((artist) => {
+        if (artist){
+            response.json(artist);
+        } else {
+            response.status(404).send();
+        }
+    });
+});
+
+app.get('/api/albums/:id',function(request,response){
+    let { id } = request.params;
+    Album.findByPk(id, {
+        include:[Artist]
+    }).then((album) => {
+        if (album){
+            response.json(album);
+        } else {
+            response.status(404).send();
+        }
+    });
+});
 
 app.listen(8000);
---
+++ models/track.js
@@ -0,0 +1,16 @@
+const sequelize = require('../database/sequelize');
+const Sequelize = require('sequelize');
+
+module.exports = sequelize.define('track',{
+    id:{
+        field:'TrackId',
+        type: Sequelize.INTEGER,
+        primaryKey:true
+    },
+    title:{
+        field:'Title',
+        type:Sequelize.STRING
+    }
+},{
+    timestamps: false
+});
