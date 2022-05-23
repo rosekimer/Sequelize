1e4e1db10a805a9d477197ae9e6dde0c488bc1b7
Sam <salonso@grupopm.mx>
2022-05-23 13:31:07 -0500

    se esta modificanco app para errores

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express');
-const Sequelize = require('sequelize');
+const Playlist = require('./models/playlist');
+
+//const Sequelize = require('sequelize');
 const app = express();
-const sequelize = new Sequelize('sqlite:chinook.db');
-const Playlist = sequelize.define('playlist',{
-    id:{
-        field:'PlaylistId',
-        type: Sequelize.INTEGER,
-        primaryKey:true
-    },
-    name:{
-        field:'Name',
-        type:Sequelize.STRING
-    }
-},{
-    timestamps: false
-});
+//const sequelize = new Sequelize('sqlite:chinook.db');
 
 app.get('/api/playlists',function(request,response){
+
+    
     Playlist.findAll().then((playlists) => {
         response.json(playlists);
     });
 });
+
+app.get('/api/playlists/:id',function(request,response){
+    let { id } = request.params;
+    Playlist.findByPk(id).then((playlist) => {
+        if (playlist){
+            response.json(playlist);
+        } else {
+            response.status(404).send();
+        }
+
+        
+    });
+});
+
 
 app.listen(8000);
